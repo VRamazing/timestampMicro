0f4842c297f96370d6f89c9e90e8f8e1dd19dea7
VRamazing <r.vignesh149@gmail.com>
2017-07-26 16:55:39 +0530

    Api ready

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Timestamp Micro Service' });
 });
 
+router.get('/:time', function(req, res, next) {
+  var dtime = req.params.time;
+  var num = Number(dtime);
+  var ret = { "unix": null, "natural": null }
+  
+
+  if(isNaN(num)){
+  	var d = Date.parse(dtime)
+  	if(!isNaN(d)){
+  		num = d
+  		ret.natural = dtime;
+  		ret.unix = num;
+  	}
+  	
+  }
+  else{
+  	ret.unix = num;
+  	var d = new Date(num);
+  	d = d.toDateString();
+	ret.natural = d.substr(4,d.length);
+
+  }
+
+  res.send(ret);
+  
+});
 module.exports = router;
